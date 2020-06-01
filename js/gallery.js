669597d9cefec1aae917e9e716f46a523f5d913d
nikifor511 <deadnikifor@gmail.com>
2020-06-01 10:48:21 +0300

    nnn

--- js/gallery.js
+++ js/gallery.js
@@ -3,31 +3,32 @@
 class Gallery {
 
     constructor(images) {
-        this.images = images;
-        this.currentSlyderImage = 0;
-        this.currentModalImageID = null;
-        this.position = 0;
-        this.fullWidth = 0;
+        this.images = images;                                               // Массив картинок в слайдере
+        this.currentSlyderImage = 0;                                        // Индекс в массиве картинки, отображаемой в слайдере слева
+        this.currentModalImageID = null;                                    // ID Картинки во всплывающем окне
+        this.position = 0;                                                  // Позиция слайдера margin-left, меняется для "прокрутеки" слайдера
+        this.fullWidth = 0;                                                 // Ширина отрисованного слайдера, испрользуется в методе previous()  (1)
         let self = this;
 
         let ul = document.getElementsByClassName('images')[0];
-        for (let i = 0; i < this.images.length; i++) {
-              let li = document.createElement("li");
+        this.images.forEach(function(image) {
+            let li = document.createElement("li");
 
-              let img = document.createElement('img');
-              img.src = './images/' + this.images[i].src;
-              img.id = this.images[i].src;
-              img.onload = function() {
-                  self.fullWidth = self.fullWidth + img.clientWidth + 4;
-              }
-              img.onclick = function() {
-                  self.currentModalImageID = this.id;
-                  self.renderModal();
-                  document.getElementById('myModal').style.display = "block";
-              }
-              li.append(img);
-              ul.append(li);
-        }
+            let img = document.createElement('img');
+            img.src = './images/' + image.src;
+            img.id = image.src;
+            img.onload = function() {
+                self.fullWidth = self.fullWidth + img.clientWidth + 4;        //Подсчет (1)
+            }
+            img.onclick = function() {
+                self.currentModalImageID = this.id;
+                self.renderModal();
+                document.getElementById('myModal').style.display = "block";
+            }
+            li.append(img);
+            ul.append(li);
+            
+        });
 
     }
 
@@ -85,7 +86,7 @@ class Gallery {
         let modalCaptionChange = document.getElementById('modalCaptionChange');
         modalCaptionChange.onclick = function() {
             img.caption = modalCaptionText.value;
-            modalCaptionText.style.backgroundColor = "#9999";
+            modalCaptionText.style.backgroundColor = "#7dff9399";
         }        
         
         console.log(modalImage);
